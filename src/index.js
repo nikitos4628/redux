60352770e50b8c7ec6f2582119a5a87b7e3bd36e
nikitos4628 <soba.nikita@mail.ru>
2023-03-25 07:57:21 +0700

    change

--- src/index.js
+++ src/index.js
@@ -1,13 +1,21 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 
 const initialState = 0;
 
-ReactDOM.render(
-  <React.StrictMode>
-    <>
+ReactDOM
+	.createRoot(document.getElementById('root'))
+	.render(
+		// <React.StrictMode>
+		<></>
+		// </React.StrictMode>
+ 	);
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <>
     
-    </>
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+//     </>
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
