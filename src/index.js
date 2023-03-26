984c1e026b921c5e699d22031d6d52a2d7b1fb3c
nikitos4628 <soba.nikita@mail.ru>
2023-03-26 18:32:48 +0700

    createStore

--- src/index.js
+++ src/index.js
@@ -1,7 +1,76 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { legacy_createStore } from 'redux';
 
-const initialState = 0;
+const initialState = {value: 0};
+
+const reducer = (state = initialState, action) => {
+	switch (action.type) {
+		case 'INC':
+			return {
+				...state,
+				value: state.value + 1
+			}
+		case 'DEC':
+			return {
+				...state,
+				value: state.value - 1
+			}
+		case 'RND':
+			return {
+				...state,
+				value: state.value * action.payload
+			}
+		default:
+			return state
+	}
+
+}
+
+const store = legacy_createStore(reducer)
+
+const update = () => {
+	document.getElementById('counter').textContent = store.getState().value 
+}
+
+store.subscribe(update)
+
+const inc = () => ({type: 'INC'})
+const dec = () => ({type: 'DEC'})
+const rnd = (value) => ({type: 'RND', payload: value})
+
+
+document.getElementById('inc').addEventListener('click', () => {
+	store.dispatch(inc())
+})
+
+document.getElementById('dec').addEventListener('click', () => {
+	store.dispatch(dec())
+})
+
+document.getElementById('rnd').addEventListener('click', () => {
+	const value = Math.floor(Math.random() * 10)
+	store.dispatch(rnd(value))
+})
+
+
+
+// store.dispatch({type: 'INC'})
+// store.dispatch({type: 'INC'})
+// store.dispatch({type: 'INC'})
+
+
+
+
+
+// let state = reducer(initialState, {type: 'INC'})
+// console.log(state);
+// state = reducer(state, {type: 'INC'})
+// console.log(state);
+// state = reducer(state, {type: 'INC'})
+// console.log(state);
+// state = reducer(state, {type: 'INC'})
+// console.log(state);
 
 ReactDOM
 	.createRoot(document.getElementById('root'))
