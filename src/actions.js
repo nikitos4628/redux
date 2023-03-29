5704e918d42706f42fa5a799d6956d475a27ea3e
nikitos4628 <soba.nikita@mail.ru>
2023-03-29 09:52:31 +0700

    connect (react-redux)

--- src/actions.js
+++ src/actions.js
@@ -1,3 +1,3 @@
 export const dec = () => ({type: 'DEC'})
 export const inc = () => ({type: 'INC'})
-export const rnd = (value) => ({type: 'RND', payload: value})
+export const rnd = () => ({type: 'RND', payload: Math.floor(Math.random() * 10)})
